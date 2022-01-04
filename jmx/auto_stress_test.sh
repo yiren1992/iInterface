feafc87c2aacb7e630c65f855df8aa68b1953fc4
yiren1992 <986673660@qq.com>
2022-01-04 18:39:58 +0800

    接口功能自动化测试程序

--- jmx/auto_stress_test.sh
+++ jmx/auto_stress_test.sh
@@ -11,8 +11,7 @@ export os_type=$(uname)
 
 # 需要在系统变量中定义jmeter根目录的位置，如下
 # export jmeter_path="/your jmeter path/"
-#export jmeter_path="/usr"
-export jmeter_path="/Applications/apache-jmeter-5.4.3"
+export jmeter_path="/usr"
 
 echo "自动化压测开始"
 
